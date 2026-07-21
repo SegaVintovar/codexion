a9df075902d5bbbd64e1af55bb905fa6325ec469
vsudak <vsudak@student.codam.nl>
2026-07-21 11:37:28 +0200

    thinking on how to pass all necessary information

--- main.c
+++ main.c
@@ -1,17 +1,21 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
-int counter = 0;
+typedef struct s_dongle {
+    pthread_mutex_t mutex;
+    time_t          time;
+}   t_dongle;
 
 void *worker(void *arg)
 {
 	// printf("Doing a job\n");
-	pthread_mutex_t	*mutex;
+	t_dongle	*dongle;
 
-	mutex = (pthread_mutex_t *)arg;
-	pthread_mutex_lock(mutex);
-	counter++;
+	dongle = (t_dongle *)arg;
+	pthread_mutex_lock(dongle->mutex);
+	dongle->time = gettimeofday();
 	pthread_mutex_unlock(mutex);
 	return NULL;
 }
@@ -23,18 +27,24 @@ int main(int argc, char **argv)
 	int			join_ret_val;
 	int			amount_of_coders;
 	int			i;
+    int         amount_of_compilations;
 	pthread_mutex_t mutex;
 
 	pthread_mutex_init(&mutex, NULL);
 	amount_of_coders = atoi(argv[1]);
+    amount_of_compilations = atoi(argv[2]);
 	thread = malloc(sizeof(pthread_t) * amount_of_coders);
 	// I think this part goes under another loop that counts amount of the compilations
-	i = 0;
-	while (i < amount_of_coders)
-		pthread_create(&thread[i++], NULL, worker, (void *)&mutex);
-	i = 0;
-	while (i < amount_of_coders)
-		pthread_join(thread[i++], NULL);
+    while (amount_of_compilations)
+    {
+        i = 0;
+        while (i < amount_of_coders)
+            pthread_create(&thread[i++], NULL, worker, (void *)&mutex);
+        i = 0;
+        while (i < amount_of_coders)
+            pthread_join(thread[i++], NULL);
+        amount_of_compilations--;
+    }
 	// 
 	pthread_mutex_destroy(&mutex);
 	printf("%d\n", counter);
